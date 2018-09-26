832616c85756ecb644f518e77d7844cfe60737bf
johntheisen2011 <johntheisen2011@gmail.com>
2018-09-26 10:27:19 -0500

    test
    test

--- Original/app/src/main/java/com/twothousandelevensoftware/original/MainActivity.java
+++ Original/app/src/main/java/com/twothousandelevensoftware/original/MainActivity.java
@@ -5,9 +5,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import static com.twothousandelevensoftware.original.R.id.ButtonJJT;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,8 +21,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Button buttonjjt = (Button) findViewById(R.id.ButtonJJT);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        //buttonjjt.set
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+    public void onClick(){
+        Log.d("jjt","buttonclicked");
     }
 
     @Override
@@ -49,4 +59,7 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+    /** Called when the user touches the button */
+    public void sendMessage(View view) {
+        Log.d("JJT","DID IT");
+}}
